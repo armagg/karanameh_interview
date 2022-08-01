d1a53f8d64fec7a959c1654ce74aefcf6fc9b200
omid0390 <omid.mesgarha@gmail.com>
2022-08-01 19:32:46 +0430

    add gunicorn support

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.controller import app
 from decouple import config
 from scripts.database_migrations import *
+
+
 if __name__ == '__main__':
-    # create_unique_index_for_links()
     app.run(host=config("HOST"), port=config('PORT'))
